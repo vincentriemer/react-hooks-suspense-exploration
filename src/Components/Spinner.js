3da51dd90ce5a50e04eb0e189e28145e3d9937fd
Vincent Riemer <vincentriemer@gmail.com>
2018-11-05 15:12:44 -0500

    Make spinner more accessible

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -2,9 +2,14 @@ import "./Spinner.css";
 import React from "react";
 
 export const Spinner = props => (
-  <div className="la-ball-scale-multiple la-dark la-2x">
+  <div
+    role="alert"
+    aria-live="assertive"
+    className="la-ball-scale-multiple la-dark la-2x"
+  >
     <div />
     <div />
     <div />
+    <p className="spinner-copy">Loading...</p>
   </div>
 );
