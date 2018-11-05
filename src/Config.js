e2ca233c2de3af54b8434d55074902cc5871fae2
Vincent Riemer <vincentriemer@gmail.com>
2018-11-05 09:28:14 -0500

    Move api key config to .env

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,12 @@
+import invariant from "invariant";
+
+invariant(
+  process.env.REACT_APP_TMDB_API_KEY,
+  `Missing TMDB API key, either set the REACT_APP_TMDB_API_KEY environment variable or add it to a .env file in the root folder of this project`
+);
+
 // API Config
-export const TMDB_API_KEY = "***REMOVED***";
+export const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 export const TMDB_PLACEHOLDER_BASE_URL = "https://image.tmdb.org/t/p/w92";
 export const TMDB_IMG_BASE_URL = "https://image.tmdb.org/t/p/w780";
